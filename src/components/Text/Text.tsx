53b22c286b21f52736feefc65a35c108c804bd7d
Michael Jonathan <michaeljonathan_98@hotmail.com>
2024-01-12 16:17:56 -0300

    with ref

--- src/components/Text/Text.tsx
+++ src/components/Text/Text.tsx
@@ -1,6 +1,7 @@
 import {
   ElementType,
   PropsWithChildren,
+  ComponentPropsWithRef,
   ComponentPropsWithoutRef,
 } from "react";
 
@@ -17,6 +18,8 @@ type AsProp<C extends ElementType> = {
   as?: C;
 };
 
+type PolymorphicRef<C extends ElementType> = ComponentPropsWithRef<C>["ref"];
+
 type PropsToOmit<C extends ElementType, Props> = keyof (AsProp<C> & Props);
 
 type TextProps = { color?: Rainbow | "black" };
@@ -27,18 +30,16 @@ type PolymorphicComponentProp<
 > = PropsWithChildren<Props & AsProp<C>> &
   Omit<ComponentPropsWithoutRef<C>, PropsToOmit<C, Props>>;
 
-export const Text = <C extends ElementType = "span">({
-  as,
-  color,
-  children,
-  ...rest
-}: PolymorphicComponentProp<C, TextProps>) => {
+export const Text = <C extends ElementType = "span">(
+  { as, color, children, ...rest }: PolymorphicComponentProp<C, TextProps>,
+  ref?: PolymorphicRef<C>
+) => {
   const Component = as || "span";
 
   const style = color ? { style: { color } } : {};
 
   return (
-    <Component {...style} {...rest}>
+    <Component ref={ref} {...style} {...rest}>
       {children}
     </Component>
   );
