264235edc81480754979dd02dfd89871aeef033f
Michael Jonathan <michaeljonathan_98@hotmail.com>
2024-01-14 00:22:12 -0300

    code refactor

--- src/components/Text/Text.tsx
+++ src/components/Text/Text.tsx
@@ -1,9 +1,3 @@
-import {
-  ElementType,
-  PropsWithChildren,
-  ComponentPropsWithoutRef,
-} from "react";
-
 type Rainbow =
   | "red"
   | "orange"
@@ -13,32 +7,32 @@ type Rainbow =
   | "indigo"
   | "violet";
 
-type AsProp<C extends ElementType> = {
+type AsProp<C extends React.ElementType> = {
   as?: C;
 };
 
-type PropsToOmit<C extends ElementType, Props> = keyof (AsProp<C> & Props);
-
-type TextProps = { color?: Rainbow | "black" };
+type PropsToOmit<C extends React.ElementType, Props> = keyof (AsProp<C> &
+  Props);
 
 type PolymorphicComponentProp<
-  C extends ElementType,
+  C extends React.ElementType,
   Props = {}
-> = PropsWithChildren<Props & AsProp<C>> &
-  Omit<ComponentPropsWithoutRef<C>, PropsToOmit<C, Props>>;
+> = React.PropsWithChildren<AsProp<C> & Props> &
+  Omit<React.ComponentPropsWithoutRef<C>, PropsToOmit<C, Props>>;
 
-export const Text = <C extends ElementType = "span">(
-  props: PolymorphicComponentProp<C, TextProps>
+type TextProps<C extends React.ElementType> = PolymorphicComponentProp<
+  C,
+  { color?: Rainbow | "black" }
+>;
+
+export const Text = <C extends React.ElementType = "span">(
+  props: TextProps<C>
 ) => {
-  const { as, color, children, ...rest } = props;
+  const { as, color, ...rest } = props;
 
   const Component = as || "span";
 
   const style = color ? { style: { color } } : {};
 
-  return (
-    <Component {...style} {...rest}>
-      {children}
-    </Component>
-  );
+  return <Component {...style} {...rest} />;
 };
